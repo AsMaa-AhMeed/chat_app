ceb1873bbfc1a91ec1aa50118a018a26164f86b3
AsMaa-AhMeed <ssemo8385@gmail.com>
2023-06-23 21:39:11 +0300

    triggering login cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:chat_app_2/components/firebase_options.dart';
+import 'package:chat_app_2/cubits/Login_cubit/login_cubit.dart';
 import 'package:chat_app_2/screens/screens.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,14 +22,17 @@ class MyApp extends StatelessWidget {
     SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
     ));
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: {
-        LoginScreen.id: (context) => LoginScreen(),
-        SignupScreen.id: (context) => SignupScreen(),
-        ChatScreen.id: (context) => ChatScreen(),
-      },
-      initialRoute: LoginScreen.id,
+    return BlocProvider(
+      create: (context) => LoginCubit(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        routes: {
+          LoginScreen.id: (context) => LoginScreen(),
+          SignupScreen.id: (context) => SignupScreen(),
+          ChatScreen.id: (context) => ChatScreen(),
+        },
+        initialRoute: LoginScreen.id,
+      ),
     );
   }
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,112 +1,102 @@
+import 'package:chat_app_2/cubits/Login_cubit/login_cubit.dart';
 import 'package:chat_app_2/helper/show_snack_bar.dart';
 import 'package:chat_app_2/screens/screens.dart';
 import 'package:chat_app_2/services/firebase_auth.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lottie/lottie.dart';
 import 'package:flutter/material.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import '../components/components.dart';
 
-class LoginScreen extends StatefulWidget {
-  static String id = '/loginRoute';
-
-  LoginScreen({super.key});
-
-  @override
-  State<LoginScreen> createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
+class LoginScreen extends StatelessWidget {
   String? email;
   String? password;
   bool isLoading = false;
+  static String id = '/loginRoute';
   GlobalKey<FormState> formKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
-    return ModalProgressHUD(
-      inAsyncCall: isLoading,
-      child: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: ListView(
-            children: [
-              Form(
-                key: formKey,
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Lottie.network(
-                          'https://assets8.lottiefiles.com/private_files/lf30_iraugwwv.json',
-                          height: 300),
-                      Text('LOGIN',
-                          style: TextStyle(
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                              color: ColorManager.blue)),
-                      const SizedBox(height: 14),
-                      CustomTextFormField(
-                        onChanged: (data) {
-                          email = data;
-                        },
-                        hintText: 'Email',
-                        keyboardType: TextInputType.emailAddress,
-                      ),
-                      const SizedBox(height: 14),
-                      CustomTextFormField(
-                        onChanged: (data) {
-                          password = data;
-                        },
-                        hintText: 'Password',
-                        keyboardType: TextInputType.visiblePassword,
-                      ),
-                      const SizedBox(height: 14),
-                      CustomTextButton(
-                        data: 'Login',
-                        onTap: () async {
-                          if (formKey.currentState!.validate()) {
-                            isLoading = true;
-                            setState(() {});
-                            try {
-                              await signInUser(
-                                  email: email, password: password);
-                              Navigator.pushNamed(context, ChatScreen.id,
-                                  arguments: email);
-                            } on FirebaseAuthException catch (e) {
-                              if (e.code == 'user-not-found') {
-                                showSnackBar(
-                                    context, 'No user found for that email.');
-                              } else if (e.code == 'wrong-password') {
-                                showSnackBar(context, 'Wrong password.');
-                              }
-                            } catch (e) {
-                              showSnackBar(context, 'There was an error!.');
+    return BlocConsumer<LoginCubit, LoginState>(
+      listener: (context, state) {
+        if (state is LoginLoading) {
+          isLoading = true;
+        } else if (state is LoginSuccess) {
+          Navigator.pushNamed(context, ChatScreen.id);
+           isLoading = false;
+        } else if (state is LoginFailure) {
+          showSnackBar(context, state.errMessage);
+           isLoading = false;
+        }
+      },
+      builder: (context, state) =>  ModalProgressHUD(
+        inAsyncCall: isLoading,
+        child: Scaffold(
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: ListView(
+              children: [
+                Form(
+                  key: formKey,
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Lottie.network(
+                            'https://assets8.lottiefiles.com/private_files/lf30_iraugwwv.json',
+                            height: 300),
+                        Text('LOGIN',
+                            style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                                color: ColorManager.blue)),
+                        const SizedBox(height: 14),
+                        CustomTextFormField(
+                          onChanged: (data) {
+                            email = data;
+                          },
+                          hintText: 'Email',
+                          keyboardType: TextInputType.emailAddress,
+                        ),
+                        const SizedBox(height: 14),
+                        CustomTextFormField(
+                          onChanged: (data) {
+                            password = data;
+                          },
+                          hintText: 'Password',
+                          keyboardType: TextInputType.visiblePassword,
+                        ),
+                        const SizedBox(height: 14),
+                        CustomTextButton(
+                          data: 'Login',
+                          onTap: () async {
+                            if (formKey.currentState!.validate()) {
+                              BlocProvider.of<LoginCubit>(context).signInUser(
+                                  email: email!, password: password!);
                             }
-                            isLoading = false;
-                            setState(() {});
-                          }
-                        },
-                      ),
-                      const SizedBox(height: 14),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Text('Don\'t have an account?  '),
-                          InkWell(
-                            onTap: () {
-                              Navigator.pushNamed(context, SignupScreen.id);
-                            },
-                            child: Text('Register',
-                                style: TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.w500,
-                                    color: ColorManager.blue)),
-                          ),
-                        ],
-                      )
-                    ]),
-              ),
-            ],
+                          },
+                        ),
+                        const SizedBox(height: 14),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text('Don\'t have an account?  '),
+                            InkWell(
+                              onTap: () {
+                                Navigator.pushNamed(context, SignupScreen.id);
+                              },
+                              child: Text('Register',
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w500,
+                                      color: ColorManager.blue)),
+                            ),
+                          ],
+                        )
+                      ]),
+                ),
+              ],
+            ),
           ),
         ),
       ),
